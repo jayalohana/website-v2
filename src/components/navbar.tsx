82c26687a6ff8ac808c752c7bf6a0c5a360810c9
Jaya <jaya.lohana24@gmail.com>
2024-04-29 14:05:57 -0400

    changes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className=" text-black p-4">
+      <ul className="flex justify-center">
+        <li className="mx-6 hover:text-gray-300 cursor-pointer transform hover:scale-125 transition duration-150 ease-in-out">
+          Home
+        </li>
+        <li className="mx-6 hover:text-gray-300 cursor-pointer transform hover:scale-125 transition duration-150 ease-in-out">
+          Projects
+        </li>
+        <li className="mx-6 hover:text-gray-300 cursor-pointer transform hover:scale-125 transition duration-150 ease-in-out">
+          Experience
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
