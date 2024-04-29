a8d96d3880b9ef3ef5422c8e45e5528853518689
Jaya <jaya.lohana24@gmail.com>
2024-04-28 21:26:50 -0400

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import About from "@/components/about";
+import Navbar from "@/components/navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="m-20">
+      <Navbar />
+      <About />
+    </div>
+  );
+}
